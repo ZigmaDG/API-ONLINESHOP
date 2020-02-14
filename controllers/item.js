6a3aa114e0e2f37c1cf355af44ecd2f42f8f7f90
Guillermo Ramos <guillermo.ramos.r95@gmail.com>
2020-02-14 11:02:34 -0600

    All method were developed, time to go to the Frontend

--- controllers/item.js
+++ controllers/item.js
@@ -3,6 +3,8 @@
 
 var validator = require('validator');
 var Item = require('../models/item');
+var fs = require('fs');
+var path = require('path');
 
 
 var controller = {
@@ -31,7 +33,7 @@ var controller = {
         }
 
         if (validate_name && validate_category && validate_price && validate_brand && validate_description) {
-           
+
             //Create the object to save
 
             var item = new Item();
@@ -64,52 +66,52 @@ var controller = {
             });
 
 
-        }else{
+        } else {
             return res.status(200).send({
                 status: 'error',
                 message: 'los datos no son válidos'
             })
         }
     },
-    
+
     // get all items stored in the DB
     getItems: (req, res) => {
-            var query = Item.find({})
+        var query = Item.find({})
 
-            var last = req.params.last;
+        var last = req.params.last;
 
-            if(last || last != undefined){
-                query.limit(5);
+        if (last || last != undefined) {
+            query.limit(5);
+        }
+
+        query.sort('-_id').exec((err, items) => {
+
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'error al devolver los productos'
+                });
             }
 
-            query.sort('-_id').exec((err, items)=>{
 
-                if(err){
-                    return res.status(500).send({
-                        status:'error',
-                        message:'error al devolver los productos'
-                    });
-                }
-
-
-                if(!items){
-                    return res.status(404).send({
-                        status: 'error',
-                        message:'no hay productos que mostrar!!!'
-                    });
-                }
-
-                return res.status(200).send({
-                    status: 'success',
-                    items
+            if (!items) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'no hay productos que mostrar!!!'
                 });
+            }
 
+            return res.status(200).send({
+                status: 'success',
+                items
             });
+
+        });
     },
 
 
     // get a specific item
-    getItem: (req, res) =>{
+    getItem: (req, res) => {
 
         //get the id through the url
 
@@ -118,7 +120,7 @@ var controller = {
 
         // verify if the item exist
 
-        if(!itemId || itemId == null) {
+        if (!itemId || itemId == null) {
             return res.status(404).send({
                 status: 'error',
                 message: 'el producto no existe!!'
@@ -127,9 +129,9 @@ var controller = {
 
         //search the item
 
-        Item.findById(itemId,(err,item) =>{
+        Item.findById(itemId, (err, item) => {
 
-            if(err || !item){
+            if (err || !item) {
                 return res.status(404).send({
                     status: 'error',
                     message: 'el producto no existe'
@@ -150,7 +152,7 @@ var controller = {
 
     // Update a specific item
 
-    update: (req,res ) =>{
+    update: (req, res) => {
 
         //get the id through the URL
 
@@ -158,17 +160,17 @@ var controller = {
 
         //get the data that arrives by put
 
-        var params =req.body;
+        var params = req.body;
 
         // validate the data
 
-        try{
+        try {
             var validate_name = !validator.isEmpty(params.name);
             var validate_category = !validator.isEmpty(params.category);
             var validate_price = !validator.isEmpty(params.price);
             var validate_brand = !validator.isEmpty(params.brand);
             var validate_description = !validator.isEmpty(params.description);
-        }catch(err){
+        } catch (err) {
             return res.status(404).send({
                 status: 'error',
                 message: 'faltan datos por enviar!'
@@ -176,19 +178,18 @@ var controller = {
 
         }
 
-        if(validate_name && validate_category && validate_price && validate_brand && validate_description)
-        {
-           
+        if (validate_name && validate_category && validate_price && validate_brand && validate_description) {
+
             // Find and update
-            Item.findByIdAndUpdate({_id: itemId}, params, {new:true}, (err, itemUpdated)=>{
-                if(err){
+            Item.findByIdAndUpdate({ _id: itemId }, params, { new: true }, (err, itemUpdated) => {
+                if (err) {
                     return res.status(500).send({
                         status: 'error',
                         message: 'Error al actualizar!!'
                     });
                 }
 
-                if(!itemUpdated){
+                if (!itemUpdated) {
                     return res.status(404).send({
                         status: 'error',
                         message: 'No existe el producto'
@@ -203,7 +204,7 @@ var controller = {
 
 
 
-        }else{
+        } else {
             return res.status(200).send({
                 status: 'success',
                 message: 'la validación no ha sido correcta!'
@@ -212,7 +213,9 @@ var controller = {
 
     },
 
-    delete: (req, res) =>{
+    // Delete a specific item
+
+    delete: (req, res) => {
 
         // get the item id through URL
 
@@ -220,8 +223,8 @@ var controller = {
 
         // Find and delete
 
-        Item.findOneAndDelete({_id: itemId}, (err, itemRemoved)=>{
-            if(err){
+        Item.findOneAndDelete({ _id: itemId }, (err, itemRemoved) => {
+            if (err) {
                 return res.status(500).send({
                     status: 'error',
                     message: 'Error al borra!!'
@@ -229,7 +232,7 @@ var controller = {
 
             }
 
-            if(!itemRemoved){
+            if (!itemRemoved) {
                 return res.status(404).send({
                     status: 'error',
                     message: 'El producto no existe'
@@ -244,6 +247,142 @@ var controller = {
         });
 
     },
+
+    //upload an image
+
+    upload: (req, res) => {
+        //get the file
+
+        var file_name = 'Imágen no subida...';
+
+        if (!req.files) {
+            return res.status(404).send({
+                status: 'error',
+                message: file_name
+            });
+
+        }
+
+        // Get file's name 
+
+        var file_path = req.files.file0.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        // Get file's extension
+
+        var extension_split = file_name.split('\.');
+        var file_ext = extension_split[1];
+
+
+        // verify extension, only images are allowed
+
+        if (file_ext != 'png' && file_ext != 'jpg' && file_ext != 'gif' && file_ext != 'jpeg' && file_ext != 'PNG' && file_ext != 'JPG' && file_ext != 'GIF' && file_ext != 'JPEG') {
+            // delete the file
+
+            fs.unlink(file_path, (err) => {
+                return res.status(200).send({
+                    status: 'error',
+                    message: 'la extensión de la imagen no es válida!!'
+                });
+            });
+        } else {
+
+            // all its good
+
+            var itemId = req.params.id;
+
+            if (itemId) {
+                //Search the file, asign file's name and update it
+
+                Item.findByIdAndUpdate({ _id: itemId }, { image: file_name }, { new: true },
+                    (err, itemUpdated) => {
+
+                        if (err || !itemUpdated) {
+                            return res.status(404).send({
+                                status: 'error',
+                                message: 'error al subir la imágen!!'
+                            });
+                        }
+
+                        return res.status(200).send({
+                            status: 'success',
+                            item: itemUpdated
+                        });
+                    });
+            } else {
+                    return res.status(200).send({
+                        status: 'success',
+                        image: file_name
+                    });
+                 }
+        }
+
+    },
+
+
+    getImage: (req,res) => {
+        
+        //get file
+
+        var file = req.params.image;
+        var path_file= './upload/items/' + file;
+
+        fs.exists(path_file, (exist) =>{
+            console.log (exist);
+
+
+            if(exist){
+                return res.sendFile(path.resolve(path_file));
+            }
+            else{
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'La imágen no existe'
+                });
+            }
+        });
+    },
+
+    search: (req,res) =>{
+        
+
+        //Get the string to search
+        var searchString = req.params.search;
+
+        // Find or
+
+        Item.find({"$or" :[
+            {"name": { "$regex": searchString, "$options": "i"}},
+            {"category": {"$regex":searchString, "$options": "i"}},
+
+        ]})
+        .sort([['price', 'descending']])
+        .exec((err,items)=>{
+
+            if(err){
+                return res.status(200).send({
+                    status: 'error',
+                    message: 'Error en la petición'
+                });
+            }
+
+            if(!items || items.length <= 0){
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No hay productos que coincidan con la búsqueda'
+                });
+            }
+
+
+            return res.status(200).send({
+                status: 'success',
+                items
+            });
+
+        });
+
+    }
 
 }//End controller
 
--- routes/item.js
+++ routes/item.js
@@ -5,6 +5,9 @@ var ItemController = require('../controllers/item');
 
 var router = express.Router();
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './upload/items'});
+
 
 // useful Routes
 
@@ -13,5 +16,8 @@ router.get('/products/:last?', ItemController.getItems);
 router.get('/product/:id', ItemController.getItem);
 router.put('/product/:id', ItemController.update);
 router.delete('/product/:id', ItemController.delete);
+router.post('/upload-image/:id', md_upload, ItemController.upload);
+router.get('/get-image/:image', ItemController.getImage);
+router.get('/search/:search', ItemController.search);
 
 module.exports = router;
